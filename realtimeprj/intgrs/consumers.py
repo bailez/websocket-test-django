610e8daceda9455ca919b930d58936d2378248b6
bailez <bailez@usp.br>
2021-12-16 12:16:20 -0300

    add async

--- realtimeprj/intgrs/consumers.py
+++ realtimeprj/intgrs/consumers.py
@@ -1,14 +1,20 @@
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer, AsyncConsumer
 import json
 from random import randint
 from time import sleep
+import asyncio
 
-class WSConsumer(WebsocketConsumer):
-    
-    def connect(self):
-        self.accept()
-    
-    def receive(self, text_data):
+class WSConsumer(AsyncConsumer):
+
+    async def websocket_connect(self, _):
+        await self.send({
+            "type": "websocket.accept",
+        })
+
+    async def websocket_receive(self, text_data):
         for i in range(1000):
-            self.send(json.dumps({'message' : randint(1,100)}))
-            sleep(1)
+            await asyncio.sleep(1)
+            await self.send({
+                'type': 'websocket.send',
+                'text': json.dumps({'message' : randint(1,100)})
+            })
